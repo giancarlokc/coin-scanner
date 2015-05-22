942eddb59604dd7db24135917db2d42610e8c38e
GIANCARLO KLEMM CAMILO <gkc11@inf.ufpr.br>
2015-05-21 22:44:15 -0300

    Changed the code structure

--- coinScanner.cxx
+++ coinScanner.cxx
@@ -1,3 +1,5 @@
+/* INCLUDES */
+
 #include "itkImage.h"
 #include "itkBinaryMorphologicalClosingImageFilter.h"
 #include "itkBinaryThresholdImageFilter.h"
@@ -10,7 +12,7 @@
 #include <iostream>
 #include <stdio.h>
 
-// COIN LABELS
+/* COIN LABELS */
 #define MOEDA_1_REAL        0
 #define MOEDA_50_CENT       1
 #define MOEDA_25_CENT       2
@@ -19,7 +21,7 @@
 #define MOEDA_5_CENT        5
 #define MOEDA_5_CENT_BRONZE 6
 
-// COIN PIXEL AMOUNTS
+/* COIN PIXEL AMOUNTS */
 #define MOEDA_1_REAL_PIXEL          117000
 #define MOEDA_50_CENT_PIXEL         83000
 #define MOEDA_25_CENT_PIXEL         100000
@@ -28,15 +30,85 @@
 #define MOEDA_5_CENT_PIXEL          0
 #define MOEDA_5_CENT_BRONZE_PIXEL   75000
 
-// ERROR MARGIN
+/* ERROR MARGIN */
 #define MARGEM_ERRO 0.075
 
-// ITK Definitions
+/* ITK Definitions */
 typedef itk::Image<unsigned char, 2>  ImageType;
 typedef itk::ImageFileReader<ImageType> ReaderType;
 typedef itk::BinaryThresholdImageFilter <ImageType, ImageType>  BinaryThresholdImageFilterType;
+typedef itk::BinaryCrossStructuringElement<ImageType::PixelType, ImageType::ImageDimension> StructuringElementType;
+typedef itk::BinaryMorphologicalClosingImageFilter <ImageType, ImageType, StructuringElementType> BinaryMorphologicalClosingImageFilterType;
+typedef itk::InvertIntensityImageFilter <ImageType> InvertIntensityImageFilterType;
+typedef itk::BinaryImageToLabelMapFilter<ImageType> BinaryImageToLabelMapFilterType;
 
-// Comparison using an error margin
+/* @FUNCTIONS    */
+
+/* Create reader from file */
+ReaderType::Pointer readFromFile(char* path) {
+    printf("> Reading file: %s... ", path);
+    ReaderType::Pointer reader = ReaderType::New();
+    reader->SetFileName(path);
+    reader->Update();
+    printf("[DONE]\n");
+    
+    return reader;
+}
+
+/* Binary threshold filter */
+BinaryThresholdImageFilterType::Pointer applyThresholdFilter(ImageType::Pointer src, int lowerThreshold, int upperThreshold, int insideValue, int outsideValue) {
+    printf("> Applying Threshold filter... ");
+    BinaryThresholdImageFilterType::Pointer thresholdFilter  = BinaryThresholdImageFilterType::New();
+    thresholdFilter->SetInput(src);
+    thresholdFilter->SetLowerThreshold(lowerThreshold);
+    thresholdFilter->SetUpperThreshold(upperThreshold);
+    thresholdFilter->SetInsideValue(insideValue);
+    thresholdFilter->SetOutsideValue(outsideValue);
+    printf("[DONE]\n");
+    
+    return thresholdFilter;
+}
+
+/* Binary morphological filter */
+BinaryMorphologicalClosingImageFilterType::Pointer applyMorphologicalClosingFilter(ImageType::Pointer src, int radius) {
+    printf("> Applying MorphologicalClosing filter... ");
+    // Create structure
+    StructuringElementType structuringElement;
+    structuringElement.SetRadius(radius);
+    structuringElement.CreateStructuringElement();
+    // Run filter    
+    BinaryMorphologicalClosingImageFilterType::Pointer closingFilter  = BinaryMorphologicalClosingImageFilterType::New();
+    closingFilter->SetInput(src);
+    closingFilter->SetKernel(structuringElement);
+    closingFilter->Update();
+    printf("[DONE]\n");
+    
+    return closingFilter;
+}
+
+/* Invert image */
+InvertIntensityImageFilterType::Pointer invertImage(ImageType::Pointer src, int maximum) {
+    printf("> Applying Invert filter... ");
+    InvertIntensityImageFilterType::Pointer invertIntensityFilter = InvertIntensityImageFilterType::New();
+    invertIntensityFilter->SetInput(src);
+    invertIntensityFilter->SetMaximum(maximum);
+    printf("[DONE]\n");
+    
+    return invertIntensityFilter;
+}
+
+/* Create LabelMap from image */
+BinaryImageToLabelMapFilterType::Pointer getLabelMap(ImageType::Pointer src) {
+    printf("> Creating LabelMap... ");
+    BinaryImageToLabelMapFilterType::Pointer binaryImageToLabelMapFilter = BinaryImageToLabelMapFilterType::New();
+    binaryImageToLabelMapFilter->SetInput(src);
+    binaryImageToLabelMapFilter->Update();
+    printf("[DONE]\n");
+    
+    return binaryImageToLabelMapFilter;
+}
+
+/* Comparison using an error margin */
 bool compare(long int size, long int compareValue){ 
     if(size < (compareValue + compareValue*MARGEM_ERRO) && size > (compareValue - compareValue*MARGEM_ERRO)) {
         return true;
@@ -45,7 +117,7 @@ bool compare(long int size, long int compareValue){
     }
 }
   
-// Compare a given pixel amount with the defined pixel amounts for each coin
+/* Compare a given pixel amount with the defined pixel amounts for each coin */
 char* findCoinType(long int size) {
     if(compare(size, MOEDA_1_REAL_PIXEL)) {
         return (char *) "1 REAL";
@@ -71,79 +143,38 @@ char* findCoinType(long int size) {
     return (char *) "INDEFINIDO";
 }
 
+/* @MAIN */
 int main(int argc, char *argv[]){
 
-    // Check arguments
+    /* Check arguments */
     if(argc < 2) {
         printf("Usage: %s [FILE PATH]\n", argv[0]); 
         return 1;
     }
     
-    // Declaration of the main variables
-    ImageType::Pointer image;
-    ImageType::Pointer image1;
-    
-    // Read input file
-    ReaderType::Pointer reader = ReaderType::New();
-    reader->SetFileName(argv[1]);
-    reader->Update();
+    /* Read input file */
+    ReaderType::Pointer reader = readFromFile(argv[1]);
  
-    // Use a threshold filter to create a binary image
-    int lowerThreshold = 10;
-    int upperThreshold = 100;
-    unsigned int radius = 30;
-    BinaryThresholdImageFilterType::Pointer thresholdFilter  = BinaryThresholdImageFilterType::New();
-    thresholdFilter->SetInput(reader->GetOutput());
-    thresholdFilter->SetLowerThreshold(lowerThreshold);
-    thresholdFilter->SetUpperThreshold(upperThreshold);
-    thresholdFilter->SetInsideValue(255);
-    thresholdFilter->SetOutsideValue(0);
-
-    // Send threshold output to the image
-    image = thresholdFilter->GetOutput();
+    /* Use a threshold filter to create a binary image */
+    BinaryThresholdImageFilterType::Pointer thresholdFilter  = applyThresholdFilter(reader->GetOutput(), 10, 100, 255, 0);
   
-    // Apply a binary morphological closing filter to remove noise
-    std::cout << "Radius: " << radius << std::endl;
-    typedef itk::BinaryCrossStructuringElement<ImageType::PixelType, ImageType::ImageDimension> StructuringElementType;
-    StructuringElementType structuringElement;
-    structuringElement.SetRadius(radius);
-    structuringElement.CreateStructuringElement();
-
-    typedef itk::BinaryMorphologicalClosingImageFilter <ImageType, ImageType, StructuringElementType> BinaryMorphologicalClosingImageFilterType;
-    BinaryMorphologicalClosingImageFilterType::Pointer closingFilter  = BinaryMorphologicalClosingImageFilterType::New();
-    closingFilter->SetInput(image);
-    closingFilter->SetKernel(structuringElement);
-    closingFilter->Update();
+    /* Apply a binary morphological closing filter to remove noise */
+    BinaryMorphologicalClosingImageFilterType::Pointer closingFilter = applyMorphologicalClosingFilter(thresholdFilter->GetOutput(), 30);
     
-    // Apply a reverse filter
-    typedef itk::InvertIntensityImageFilter <ImageType>
-    InvertIntensityImageFilterType;
+    /* Invert image */
+    InvertIntensityImageFilterType::Pointer invertIntensityFilter = invertImage(closingFilter->GetOutput(), 255);
 
-    InvertIntensityImageFilterType::Pointer invertIntensityFilter
-    = InvertIntensityImageFilterType::New();
-    invertIntensityFilter->SetInput(closingFilter->GetOutput());
-    invertIntensityFilter->SetMaximum(255);
+    /* Apply an imagetoLabelMap filter to separate objects */
+    BinaryImageToLabelMapFilterType::Pointer binaryImageToLabelMapFilter = getLabelMap(invertIntensityFilter->GetOutput());
 
-    // Send the morphological output to the image1
-    image1 = invertIntensityFilter->GetOutput();
-
-    // Apply an imagetoLabelMap filter to separate objects
-    typedef itk::BinaryImageToLabelMapFilter<ImageType> BinaryImageToLabelMapFilterType;
-    BinaryImageToLabelMapFilterType::Pointer binaryImageToLabelMapFilter = BinaryImageToLabelMapFilterType::New();
-    binaryImageToLabelMapFilter->SetInput(image1);
-    binaryImageToLabelMapFilter->Update();
-
-    // The output of this filter is an itk::LabelMap, which contains itk::LabelObject's
-    std::cout << "Numero de objetos: " << binaryImageToLabelMapFilter->GetOutput()->GetNumberOfLabelObjects() << std::endl;
-
-    // Loop over each region
+    /* Loop over each region in the map */
     for(unsigned int i = 0; i < binaryImageToLabelMapFilter->GetOutput()->GetNumberOfLabelObjects(); i++) {
         // Get the ith region
         BinaryImageToLabelMapFilterType::OutputImageType::LabelObjectType* labelObject = binaryImageToLabelMapFilter->GetOutput()->GetNthLabelObject(i);
-
-        // Output the number of pixels composing the region
         labelObject->Optimize();
-        printf("Objeto %10d - Tamanho: %20ld - Tipo: %20s\n", i+1, labelObject->Size(), findCoinType(labelObject->Size()));
+        
+        // Check if the object size matches any coin
+        printf("Object %10d - Size: %20ld - Type: %20s\n", i+1, labelObject->Size(), findCoinType(labelObject->Size()));
     }
 
     return EXIT_SUCCESS;
